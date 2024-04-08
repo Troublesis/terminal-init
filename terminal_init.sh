9a9e60a4f412e72716a1edadb5862aaaac22c42f
Troublesis <40723560+Troublesis@users.noreply.github.com>
2024-04-08 14:38:23 +1000

    Update terminal_init.sh

--- terminal_init.sh
+++ terminal_init.sh
@@ -39,15 +39,15 @@ if [ "$package_manager" != "Unsupported package manager. Exiting." ]; then
 
     # Install oh-my-zsh and configure plugins
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-    echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
-    sed -i 's/plugins=(git)/plugins=(git vi-mode zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
-
+    
     # Install zsh-autosuggestions and zsh-syntax-highlighting plugins
     git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-    
-    
+    sed -i 's/plugins=(git)/plugins=(git vi-mode zsh-autosuggestions zsh-syntax-highlighting)/g' ~/.zshrc
+
+     # Install oh-my-zsh powerleve110k theme
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+    echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
     
     # Reload shell configuration
     source ~/.zshrc
